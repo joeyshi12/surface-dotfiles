416bf10a750c4abb9e0115ab6fde91a1522be924
joeyshi12 <joeyshi12@gmail.com>
2021-07-19 00:19:17 -0700

    cleaned up

--- .config/bspwm/rofi/launcher/launcher.sh
+++ .config/bspwm/rofi/launcher/launcher.sh
@@ -3,14 +3,7 @@
 theme="nord"
 dir="$HOME/.config/bspwm/rofi/launcher"
 
-# Fix for keyboard-locking bug
-sleep 0.1
-
 # Launcher
 rofi -no-lazy-grab \
      -theme $dir/$theme \
-     -lines 8 \
-     -modi run,drun,window \
-     -show drun \
-     -show-icons \
-     -sidebar-mode
+     -show drun
